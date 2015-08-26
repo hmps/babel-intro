8c59c63e0e1dc4aa4fc417682f44e1e44dc0a945
Hampus Persson <h@hmps.se>
2015-08-26 09:06:56 +0200

    import and kick-off angular

--- src/es2015.js
+++ src/es2015.js
@@ -1,4 +1,10 @@
 // ES2015.JS
+import angular from 'angular';
 import { postman } from './postman';
 
 postman.logger('Hello beautiful world of ES2015, with a named import!');
+
+angular.module('babel-intro', []);
+
+// Bootstrap Angular, so we do not have to add a pesky ng-app attribute
+angular.element(document).ready(() => angular.bootstrap(document, ['babel-intro']));
